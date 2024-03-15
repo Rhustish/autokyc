95a58de13bde38937ebe17290dfa65ad2b311b16
Rhustish <bbhatnagar27@gmail.com>
2024-03-16 04:14:34 +0530

    allowed cross origin requests

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI,Response
+from fastapi.middleware.cors import CORSMiddleware
 from ImageMatching.main import compare_faces_from_urls
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/hc")
 def healthcheck():
