d8ad364bb1dfd8803909a417987159b3d2245237
Rhustish <bbhatnagar27@gmail.com>
2024-03-16 04:12:42 +0530

    added response codes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response
 from ImageMatching.main import compare_faces_from_urls
 
 app = FastAPI()
@@ -10,7 +10,8 @@ def healthcheck():
 
 @app.post("/imageComparison")
 async def imageComparison(image1:str , image2 : str):
-    return {"verified" : "1" if await compare_faces_from_urls(image1,image2) else "0" }
+    verified = "1" if await compare_faces_from_urls(image1,image2) else "0"
+    return Response(content={"verified": verified}, status_code=200)
 
 @app.post("/videoLive")
 def videoLive():
